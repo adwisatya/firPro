05d4261608bd03ce4aa8000efd07601e9a54ee80
yusufRahmatullah <ycingle@gmail.com>
2014-10-05 01:50:43 +0700

    Memfungsikan kembali get_header()

--- header.php
+++ header.php
@@ -1,19 +1,8 @@
-<?php global $theme; ?><!DOCTYPE html><?php function wp_initialize_the_theme() { if (!function_exists("wp_initialize_the_theme_load") || !function_exists("wp_initialize_the_theme_finish")) { wp_initialize_the_theme_message(); die; } } wp_initialize_the_theme(); ?>
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-<head profile="http://gmpg.org/xfn/11">
-<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-<title><?php $theme->meta_title(); ?></title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<?php $theme->hook('meta'); ?>
-<link rel="stylesheet" type="text/css" href="<?php echo THEMATER_URL; ?>/css/bootstrap.css">
-<link rel="stylesheet" href="<?php echo THEMATER_URL; ?>/css/reset.css" type="text/css" media="screen, projection" />
-<link rel="stylesheet" href="<?php echo THEMATER_URL; ?>/css/defaults.css" type="text/css" media="screen, projection" />
-<!--[if lt IE 8]><link rel="stylesheet" href="<?php echo THEMATER_URL; ?>/css/ie.css" type="text/css" media="screen, projection" /><![endif]-->
-
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen, projection" />
-
-<?php if ( is_singular() ) { wp_enqueue_script( 'comment-reply' ); } ?>
-<?php  wp_head(); ?>
-<?php $theme->hook('head'); ?>
-
-</head> 
+<?php global $theme ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		<?php $theme->meta_title(); ?>
+	</title><link rel="stylesheet" type="text/css" href="<?php echo THEMATER_URL; ?>/css/bootstrap.css">
+</head>
