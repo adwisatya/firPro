6a6ab4b5206f47a6f3b3ab3097acb809ac771d1a
adwisatya <a.dwisaty4@yahoo.com>
2014-10-14 18:04:18 +0700

    homepage news 0.5

--- index.php
+++ index.php
@@ -1,4 +1,38 @@
 <?php global $theme; get_header(); ?>
+	<div>
+		<h3>NEWS</h3>
 
+		<?php 
+			$is_post_wrap = 0;
+				if (have_posts()) : while (have_posts()) : the_post();
+				/**
+				 * The default post formatting from the post.php template file will be used.
+				 * If you want to customize the post formatting for your homepage:
+				 * 
+				 *   - Create a new file: post-homepage.php
+				 *   - Copy/Paste the content of post.php to post-homepage.php
+				 *   - Edit and customize the post-homepage.php file for your needs.
+				 * 
+				 * Learn more about the get_template_part() function: http://codex.wordpress.org/Function_Reference/get_template_part
+				 */
 
+				$is_post_wrap++;
+					if($is_post_wrap == '1') {
+						?><div><?php
+					}
+					get_template_part('post', 'homepage');
+					
+					if($is_post_wrap == '2') {
+						$is_post_wrap = 0;
+						?></div><?php
+					}
+				
+			endwhile;
+			
+			else :
+				get_template_part('post', 'noresults');
+			endif; 
+
+		?>
+	</div>
 <?php get_footer(); ?>
--- post-homepage.php
+++ post-homepage.php
@@ -1,32 +1,35 @@
 <?php global $theme; ?>
-    
-	<table border width="600px">
-		<tr>
-			<td>
-				<?php the_title(); ?>
-			</td>
-			<td>
-				<?php echo get_the_date(); ?>
-			</td>
-		</tr>
-		<tr>
-			<td>
-            <?php
-                if(has_post_thumbnail())  {
-                    ?><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(
-                        array($theme->get_option('featured_image_width'), $theme->get_option('featured_image_height')),
-                        array("class" => $theme->get_option('featured_image_position') . " featured_image")
-                    ); ?></a><?php  
-                }
-            ?>
-			</td>
-			<td>
-				<p>
-					<?php
-					  echo $theme->shorten(get_the_excerpt(), '40');
-					?>				
-				</p>
-			</td>
-		</tr>
-	</table>
+	<div id="artikel" >
+		<table>
+			<tr>
+				<td>
+					<?php the_title(); ?>
+				</td>
+				<td>
+					<p><?php echo get_the_date(); ?></p>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div id="artikel-thumbnail">
+						<?php
+							if(has_post_thumbnail())  {
+								?><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(
+									array($theme->get_option('featured_image_width'), $theme->get_option('featured_image_height')),
+									array("class" => $theme->get_option('featured_image_position') . " featured_image")
+								); ?></a><?php  
+							}
+						?>
+					</div>
+				</td>
+				<td>
+					<div id="artikel-summary">
+						<?php
+						  echo $theme->shorten(get_the_excerpt(), '250');
+						?>	
+					</div>
+				</td>
+			</tr>
+		</table>
+	</div>
     
